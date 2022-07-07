e767b0c88addc7c2e5ac29ce6a7f150fafd45139
doddatpivotal <dpfeffer@vmware.com>
2022-07-07 11:11:33 -0400

    Updates build process

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,4 +2,6 @@ pushd employee-todo-list-app
 npm install
 npm run build
 popd
+rm -rf employee-todo-list-api/wwwroot
+cp -R employee-todo-list-app/dist employee-todo-list-api/wwwroot
 dotnet build employee-todo-list-api
